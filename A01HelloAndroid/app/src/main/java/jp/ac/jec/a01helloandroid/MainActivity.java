0a3d1bc917dfdabfd65dd13ce4e720c2d0c9fe8f
LeoAndo <leo.777.info@gmail.com>
2026-09-15 10:50:35 +0900

    javaのコード修正

--- A01HelloAndroid/app/src/main/java/jp/ac/jec/a01helloandroid/MainActivity.java
+++ A01HelloAndroid/app/src/main/java/jp/ac/jec/a01helloandroid/MainActivity.java
@@ -1,12 +1,18 @@
 package jp.ac.jec.a01helloandroid;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        var text = (TextView) findViewById(R.id.text);
+        var button = (Button) findViewById(R.id.button);
+        var btn1 = (Button) findViewById(R.id.btn1);
+        var btn2 = (Button) findViewById(R.id.btn2);
+
+        var string = text.getText().toString();
+        Log.d("MainActivity", string);
+
+        button.setOnClickListener(v -> {
+            text.setText("Hello, Android!");
+            var updateString = text.getText().toString();
+            Log.d("MainActivity", updateString);
+        });
+
+        btn1.setOnClickListener(v -> {
+            var btn1Str = btn1.getText().toString();
+            Snackbar.make(findViewById(R.id.main), btn1Str, Snackbar.LENGTH_SHORT).show();
+        });
+        btn2.setOnClickListener(v -> {
+            var btn2Str = btn2.getText().toString();
+            Toast.makeText(this, btn2Str, Toast.LENGTH_SHORT).show();
         });
     }
 }
